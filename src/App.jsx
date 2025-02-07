ca46681e0773a0d9f0a75118828ed6fc07a84b1e
VictorFarfan98 <vafm98@hotmail.com>
2025-02-07 12:32:07 -0600

    updated text to spanish and personalized it

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import lovesvg from "./assets/All You Need Is Love SVG Cut File.svg";
-import lovesvg2 from "./assets/Love In The Air SVG Cut File.svg";
 
 export default function Page() {
   const [noCount, setNoCount] = useState(0);
@@ -12,29 +10,29 @@ export default function Page() {
   };
 
   const getNoButtonText = () => {
-    const phrases = [
+    const spanishPhrases = [
       "No",
-      "Are you sure?",
-      "Really sure?",
-      "Think again!",
-      "Last chance!",
-      "Surely not?",
-      "You might regret this!",
-      "Give it another thought!",
-      "Are you absolutely certain?",
-      "This could be a mistake!",
-      "Have a heart!",
-      "Don't be so cold!",
-      "Change of heart?",
-      "Wouldn't you reconsider?",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
-      "Plsss? :( You're breaking my heart",
+      "¿Estás segura?",
+      "¿De verdad?",
+      "¡Piénsalo de nuevo!",
+      "¡Última oportunidad!",
+      "¿Segura que no?",
+      "¡Podrías arrepentirte!",
+      "¡Vuelve a pensarlo!",
+      "¿Estás absolutamente segura?",
+      "¡Podría ser un error!",
+      "¡Ten corazón!",
+      "¡No seas tan fría!",
+      "¿Cambias de opinión?",
+      "¿No lo reconsiderarías?",
+      "¿Esa es tu respuesta final?",
+      "Me estás rompiendo el corazón ;(",
+      "¿Esa es tu respuesta final?",
+      "Me estás rompiendo el corazón ;(",
+      "Por favor? :( Me estás rompiendo el corazón",
     ];
 
-    return phrases[Math.min(noCount, phrases.length - 1)];
+    return spanishPhrases[Math.min(noCount, spanishPhrases.length - 1)];
   };
 
   return (
@@ -43,25 +41,23 @@ export default function Page() {
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
           <div className="text-4xl md:text-6xl font-bold my-4">
-            Ok Yayyyyy!!!
+            Wujuuuuuuu!!!
+          </div>
+          <div className="text-4xl md:text-6xl font-bold my-4" style={{ color: "#E30B5C" }}>
+            Nos vemos el viernes 14! Te quiero!!!
           </div>
         </>
       ) : (
         <>
-          <img
-            src={lovesvg}
-            className="fixed animate-pulse top-10 md:left-24 left-6 md:w-40 w-28"
-          />
-          <img
-            src={lovesvg2}
-            className="fixed bottom-16 -z-10 animate-pulse md:right-24 right-10 md:w-40 w-32"
-          />
+        <h1 className="text-4xl md:text-6xl my-4 text-center" style={{ color: "#E30B5C" }}>
+            Kenia Eunice Solis Rodriguez
+          </h1>
           <img
             className="h-[230px] rounded-lg shadow-lg"
             src="https://gifdb.com/images/high/cute-Love-bear-roses-ou7zho5oosxnpo6k.gif"
           />
           <h1 className="text-4xl md:text-6xl my-4 text-center">
-            Will you be my Valentine?
+            Quieres ser mi Valentine?
           </h1>
           <div className="flex flex-wrap justify-center gap-2 items-center">
             <button
@@ -69,7 +65,7 @@ export default function Page() {
               style={{ fontSize: yesButtonSize }}
               onClick={() => setYesPressed(true)}
             >
-              Yes
+              Si
             </button>
             <button
               onClick={handleNoClick}
@@ -87,15 +83,6 @@ export default function Page() {
 
 const Footer = () => {
   return (
-    <a
-      className="fixed bottom-2 right-2 backdrop-blur-md opacity-80 hover:opacity-95 border p-1 rounded border-rose-300"
-      href="https://github.com/Xeven777/valentine"
-      target="__blank"
-    >
-      Made with{" "}
-      <span role="img" aria-label="heart">
-        ❤️
-      </span>
-    </a>
+    <a></a>
   );
 };
